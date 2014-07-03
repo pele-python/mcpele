653a7706500c51ca65baef801cc33e34781646b5
Stefano Martiniani <stefano.martiniani@gmail.com>
2014-07-03 15:10:22 +0100

    dapted tests
    cpp tests have been adapted and tested for memory errors in valgrind, the code is clean

--- cpp_tests/source/test_mc.cpp
+++ cpp_tests/source/test_mc.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <gtest/gtest.h>
+#include <memory>
 
 #include "pele/harmonic.h"
 
@@ -14,6 +15,8 @@
 #include "mcpele/mc.h"
 
 #define EXPECT_NEAR_RELATIVE(A, B, T)  EXPECT_NEAR(fabs(A)/(fabs(A)+fabs(B)+1), fabs(B)/(fabs(A)+fabs(B)+1), T)
+
+using std::shared_ptr;
 
 class TestMC: public ::testing::Test{
 public:
@@ -52,12 +55,12 @@ TEST_F(TestMC, BasicFunctionalityAddingModulesStatic){
     mcpele::MC mc(potential, x, 1, stepsize);
     EXPECT_TRUE( k == potential->get_k() );
     EXPECT_TRUE( k == reinterpret_cast<pele::Harmonic*>(mc.get_potential_ptr())->get_k() );
-    mcpele::RandomCoordsDisplacement sampler_uniform;
+    shared_ptr<mcpele::RandomCoordsDisplacement> sampler_uniform = std::make_shared<mcpele::RandomCoordsDisplacement>();
     EXPECT_TRUE( !mc.take_step_specified() );
-    mc.set_takestep(&sampler_uniform);
+    mc.set_takestep(sampler_uniform);
     EXPECT_TRUE( mc.take_step_specified() );
-    mcpele::MetropolisTest metropolis(42);
-    mc.add_accept_test(&metropolis);
+    shared_ptr<mcpele::MetropolisTest> metropolis = std::make_shared<mcpele::MetropolisTest>(42);
+    mc.add_accept_test(metropolis);
     //mc.set_print_progress(true);
     mc.run(max_iter);
 }
@@ -66,17 +69,15 @@ TEST_F(TestMC, BasicFunctionalityAddingModulesDynamic){
     mcpele::MC* mc = new mcpele::MC(potential, x, 1, stepsize);
     EXPECT_TRUE( k == potential->get_k() );
     EXPECT_TRUE( k == reinterpret_cast<pele::Harmonic*>(mc->get_potential_ptr())->get_k() );
-    mcpele::RandomCoordsDisplacement* sampler_uniform = new mcpele::RandomCoordsDisplacement;
+    shared_ptr<mcpele::RandomCoordsDisplacement> sampler_uniform = std::make_shared<mcpele::RandomCoordsDisplacement>();
     EXPECT_TRUE( !mc->take_step_specified() );
     mc->set_takestep(sampler_uniform);
     EXPECT_TRUE( mc->take_step_specified() );
-    mcpele::MetropolisTest* metropolis = new mcpele::MetropolisTest(42);
+    shared_ptr<mcpele::MetropolisTest> metropolis = std::make_shared<mcpele::MetropolisTest>(42);
     mc->add_accept_test(metropolis);
     //mc->set_print_progress(true);
     mc->run(max_iter);
     delete mc;
-    delete sampler_uniform;
-    delete metropolis;
 }
 
 TEST_F(TestMC, BasicFunctionalityPolyHarmonic){
@@ -84,18 +85,18 @@ TEST_F(TestMC, BasicFunctionalityPolyHarmonic){
     EXPECT_TRUE( k == potential->get_k() );
     EXPECT_TRUE( k == static_cast<pele::Harmonic*>(mc->get_potential_ptr())->get_k() );
     //std::cout << "initial step size: " << mc->get_stepsize() << std::endl;
-    mcpele::TakeStep* sampler_uniform = new mcpele::RandomCoordsDisplacement;
+    shared_ptr<mcpele::TakeStep> sampler_uniform = std::make_shared<mcpele::RandomCoordsDisplacement>();
     EXPECT_TRUE( !mc->take_step_specified() );
     mc->set_takestep(sampler_uniform);
     EXPECT_TRUE( mc->take_step_specified() );
-    mcpele::AcceptTest* metropolis = new mcpele::MetropolisTest(42);
+    shared_ptr<mcpele::AcceptTest> metropolis = std::make_shared<mcpele::MetropolisTest>(42);
     mc->add_accept_test(metropolis);
     //AdjustStep(double target, double factor, size_t niter, size_t navg)
     const size_t adj_iter(max_iter/1e1);
-    mcpele::Action* adjust_step = new mcpele::AdjustStep(0.2, 0.5, adj_iter, adj_iter/1e1);
+    shared_ptr<mcpele::Action> adjust_step = std::make_shared<mcpele::AdjustStep>(0.2, 0.5, adj_iter, adj_iter/1e1);
     mc->add_action(adjust_step);
     //(double min, double max, double bin, size_t eqsteps)
-    mcpele::Action* record_histogram = new mcpele::RecordEnergyHistogram(0,10,1,adj_iter);
+    shared_ptr<mcpele::Action> record_histogram = std::make_shared<mcpele::RecordEnergyHistogram>(0,10,1,adj_iter);
     mc->add_action(record_histogram);
     //mc->set_print_progress(true);
     mc->run(max_iter);
@@ -105,15 +106,11 @@ TEST_F(TestMC, BasicFunctionalityPolyHarmonic){
     //std::cout << sqrt(static_cast<mcpele::RecordEnergyHistogram*>(record_histogram)->get_variance()) << std::endl;
     //std::cout << "prediced mean energy: " << std::endl;
     //std::cout << 0.5*ndof << std::endl;
-    const double computed_mean = static_cast<mcpele::RecordEnergyHistogram*>(record_histogram)->get_mean();
-    const double computed_std = sqrt(static_cast<mcpele::RecordEnergyHistogram*>(record_histogram)->get_variance());
+    const double computed_mean = std::static_pointer_cast<mcpele::RecordEnergyHistogram>(record_histogram)->get_mean();
+    const double computed_std = sqrt(std::static_pointer_cast<mcpele::RecordEnergyHistogram>(record_histogram)->get_variance());
     const double expected_mean = 0.5*ndof;
     EXPECT_NEAR(computed_mean, expected_mean, computed_std);
     //std::cout << "acc frac: " << mc->get_accepted_fraction() << std::endl;
     //std::cout << "final step size: " << mc->get_stepsize() << std::endl;
     delete mc;
-    delete sampler_uniform;
-    delete metropolis;
-    delete adjust_step;
-    delete record_histogram;
 }
