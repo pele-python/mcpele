f40dc26b4c64d6a9d940a12e7ee1566f689f1b5a
Stefano Martiniani <stefano.martiniani@gmail.com>
2014-07-07 15:59:00 +0100

    fixed metropolis tests

--- mcpele/monte_carlo/tests/test_metropolis_mcrunner.py
+++ mcpele/monte_carlo/tests/test_metropolis_mcrunner.py
@@ -9,36 +9,114 @@ import logging
 
 class TestMetropolis(unittest.TestCase):
     
-    def test_heat_capacity(self):
-        nparticles=5
-        self.ndim = nparticles*3
+    def setUp(self):
+        self.natoms = 4
         self.k=1
-        self.origin = np.zeros(self.ndim)
-        self.potential = Harmonic(self.origin,self.k,com=True)
         self.Emax = 2 #this choice is fundamentally arbitrary, it's only used to generate the initial configuration
-        self.start_coords = vector_random_uniform_hypersphere(self.ndim) * np.sqrt(2*self.Emax) #coordinates sampled from Pow(ndim)
-               
-        temperatures = [0.2,0.27,0.362,0.487,0.65,0.88,1.18,1.6]
-                
-        for T in temperatures:
-            temperature=T
-            stepsize=0.5
-            niter=3e6
-            mcrunner = Metropolis_MCrunner(self.potential, self.start_coords, temperature, stepsize, niter, hEmax = 100, adjustf = 0.9, 
-                                           adjustf_niter = 10000, radius=10000000)
+        #self.temperatures = [0.2,0.27,0.362,0.487,0.65,0.88,1.18,1.6]
+        self.temperatures = [0.2,0.362,0.65,1.18]
+        self.stepsize=1
+        self.niter=2e6
+        self.adjustf = 0.9
+        self.adjust_niter = 0.3*self.niter
+        self.radius = 1e10
+            
+    def test_heat_capacity_3D_com(self):
+        self.bdim = 3
+        self.ndim = self.natoms*self.bdim
+        self.origin = np.zeros(self.ndim)
+        potential = Harmonic(self.origin, self.k, bdim=self.bdim, com=True)
+                        
+        print "3D com"
+        for T in self.temperatures:
+                        
+            mcrunner = Metropolis_MCrunner(potential, self.origin, T, self.stepsize, self.niter, hEmax = 100, 
+                                           adjustf = self.adjustf, adjustf_niter = self.adjust_niter, radius=self.radius, 
+                                           bdim=self.bdim)
             #MCMC 
             mcrunner.run()
-            
             #collect the results
-            binenergy, hist = mcrunner.get_histogram()
+            binenergy, hist, mean, variance = mcrunner.get_histogram()
             
-            average = np.average(binenergy,weights=hist)
-                
-            average2 = np.average(np.square(binenergy),weights=hist)
+            cv =  variance/(T*T)
+            cv_true = (self.natoms-1)*self.bdim/2.0
             
-            cv =  (average2 - average**2)/(T**2) #+ self.ndim/2
+            print cv, cv_true
             
-            self.assertLess(abs(cv-(self.ndim-3)/2.0),1e-1,'failed for temperature {}, cv = {}'.format(T,cv))
+            self.assertLess(abs(cv-cv_true),1e-1,'failed for temperature {}, cv = {}'.format(T,cv))
+    
+    def test_heat_capacity_3D(self):
+        self.bdim = 3
+        self.ndim = self.natoms*self.bdim
+        self.origin = np.zeros(self.ndim)
+        potential = Harmonic(self.origin, self.k, bdim=self.bdim, com=False)
+        
+        print "3D"
+        for T in self.temperatures:
+                        
+            mcrunner = Metropolis_MCrunner(potential, self.origin, T, self.stepsize, self.niter, hEmax = 100, 
+                                           adjustf = self.adjustf, adjustf_niter = self.adjust_niter, radius=self.radius, 
+                                           bdim=self.bdim)
+            #MCMC 
+            mcrunner.run()
+            #collect the results
+            binenergy, hist, mean, variance = mcrunner.get_histogram()
+            
+            cv =  variance/(T*T)
+            cv_true = self.natoms*self.bdim/2.0
+            
+            print cv, cv_true
+            
+            self.assertLess(abs(cv-cv_true),1e-1,'failed for temperature {}, cv = {}'.format(T,cv))
+    
+    def test_heat_capacity_2D_com(self):
+        self.bdim = 2
+        self.ndim = self.natoms*self.bdim
+        self.origin = np.zeros(self.ndim)
+        potential = Harmonic(self.origin,self.k, bdim=self.bdim,com=True)
+        #self.start_coords = vector_random_uniform_hypersphere(self.ndim) * np.sqrt(2*self.Emax) #coordinates sampled from Pow(ndim)
+                        
+        print "2D com"
+        for T in self.temperatures:
+                        
+            mcrunner = Metropolis_MCrunner(potential, self.origin, T, self.stepsize, self.niter, hEmax = 100, 
+                                           adjustf = self.adjustf, adjustf_niter = self.adjust_niter, radius=self.radius, 
+                                           bdim=self.bdim)
+            #MCMC 
+            mcrunner.run()
+            #collect the results
+            binenergy, hist, mean, variance = mcrunner.get_histogram()
+            
+            cv =  variance/(T*T)
+            cv_true = (self.natoms-1)*self.bdim/2.0
+            
+            print cv, cv_true
+            
+            self.assertLess(abs(cv-cv_true),1e-1,'failed for temperature {}, cv = {}'.format(T,cv))
+    
+    def test_heat_capacity_2D(self):
+        self.bdim = 2
+        self.ndim = self.natoms*self.bdim
+        self.origin = np.zeros(self.ndim)
+        self.potential = Harmonic(self.origin, self.k, bdim=self.bdim, com=False)
+                        
+        print "2D"
+        for T in self.temperatures:
+                        
+            mcrunner = Metropolis_MCrunner(self.potential, self.origin, T, self.stepsize, self.niter, hEmax = 100, 
+                                           adjustf = self.adjustf, adjustf_niter = self.adjust_niter, radius=self.radius, 
+                                           bdim=self.bdim)
+            #MCMC 
+            mcrunner.run()
+            #collect the results
+            binenergy, hist, mean, variance = mcrunner.get_histogram()
+            
+            cv =  variance/(T*T)
+            cv_true = self.natoms*self.bdim/2.0
+            
+            print cv, cv_true
+            
+            self.assertLess(abs(cv-cv_true),1e-1,'failed for temperature {}, cv = {}'.format(T,cv))
 
 if __name__ == "__main__":
     logging.basicConfig(filename='Metropolis_mcrunner.log',level=logging.DEBUG)
