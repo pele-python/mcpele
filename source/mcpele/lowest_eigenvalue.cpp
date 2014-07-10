1dbd6c1bd6c10867efe8baede86e20f11ad36358
Julian Schrenk <kjs73@cam.ac.uk>
2014-07-10 14:12:17 +0100

    make lowesteigpot and lbfgs members, use reset functions

--- source/mcpele/lowest_eigenvalue.cpp
+++ source/mcpele/lowest_eigenvalue.cpp
@@ -1,8 +1,5 @@
 #include <cmath>
 
-#include "pele/lbfgs.h"
-#include "pele/lowest_eig_potential.h"
-
 #include "lowest_eigenvalue.h"
 
 namespace mcpele{
@@ -10,27 +7,26 @@ namespace mcpele{
 
 FindLowestEigenvalue::FindLowestEigenvalue(std::shared_ptr<pele::BasePotential> landscape_potential, const size_t boxdimension,
         pele::Array<double> ranvec, const size_t lbfgsniter)
-    : _landscape_potential(landscape_potential),
-      _boxdimension(boxdimension),
-      _ranvec(ranvec.copy()),
+    : _lowesteigpot(std::make_shared<pele::LowestEigPotential>(landscape_potential, ranvec.copy(), boxdimension)),
       _lbfgsniter(lbfgsniter),
-      _H0(1)
+      _H0(1),
+      _ranvec((ranvec /= norm(ranvec)).copy()),
+      _lbfgs(_lowesteigpot, _ranvec.copy())
 {
-    if (isinf(double(1) / norm(_ranvec))) {
-        throw std::runtime_error("FindLowestEigenvalue: 1/norm(_ranvec) is isinf");
+    if (isinf(double(1) / norm(ranvec))) {
+        throw std::runtime_error("FindLowestEigenvalue: 1/norm(ranvec) is isinf");
     }
-    _ranvec /= norm(_ranvec);
 }
 
 double FindLowestEigenvalue::get_lowest_eigenvalue(pele::Array<double> coords)
 {
-    std::shared_ptr<pele::LowestEigPotential> lowesteigpot = std::make_shared<pele::LowestEigPotential>(_landscape_potential, coords, _boxdimension);
-    pele::LBFGS lbfgs(lowesteigpot, _ranvec.copy());
-    lbfgs.set_H0(_H0);
-    lbfgs.set_use_relative_f(1);
-    lbfgs.run(_lbfgsniter);
-    _H0 = lbfgs.get_H0();
-    const double lowesteig = lbfgs.get_f();
+    _lowesteigpot->reset_coords(coords);
+    _lbfgs.reset(_ranvec);
+    _lbfgs.set_H0(_H0);
+    _lbfgs.set_use_relative_f(1);
+    _lbfgs.run(_lbfgsniter);
+    _H0 = _lbfgs.get_H0();
+    const double lowesteig = _lbfgs.get_f();
     return lowesteig;
 }
 
--- source/mcpele/lowest_eigenvalue.h
+++ source/mcpele/lowest_eigenvalue.h
@@ -2,17 +2,19 @@
 #define _MCPELE_LOWEST_EIGENVALUE_H
 
 #include "pele/base_potential.h"
+#include "pele/lbfgs.h"
+#include "pele/lowest_eig_potential.h"
 
 namespace mcpele{
 
 
 class FindLowestEigenvalue{
 private:
-    std::shared_ptr<pele::BasePotential> _landscape_potential;
-    const size_t _boxdimension;
-    pele::Array<double> _ranvec;
-    const size_t _lbfgsniter;
+    std::shared_ptr<pele::LowestEigPotential> _lowesteigpot;
+    size_t _lbfgsniter;
     double _H0;
+    pele::Array<double> _ranvec;
+    pele::LBFGS _lbfgs;
 public:
     FindLowestEigenvalue(std::shared_ptr<pele::BasePotential> landscape_potential, const size_t boxdimension,
             pele::Array<double> ranvec, const size_t lbfgsniter);
