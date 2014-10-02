fbddfcb749184eeaf31e0a28499aa73387ac792e
Julian Schrenk <kjs73@cam.ac.uk>
2014-10-02 14:08:38 +0100

    add cpp part of probability composite moves

---
+++ source/mcpele/take_step_probabilities.h
@@ -0,0 +1,40 @@
+#ifndef _MCPELE_TAKE_STEP_PROBABILITIES_H__
+#define _MCPELE_TAKE_STEP_PROBABILITIES_H__
+
+#include <vector>
+#include <random>
+
+#include "mc.h"
+
+namespace mcpele {
+
+/**
+ * Create a step pattern similar to TakeStepPattern.
+ * However, the steps are specified together with their relative weights and
+ * exectured accoringly.
+ *
+ * Reference
+ * ---------
+ * http://www.cplusplus.com/reference/random/discrete_distribution/
+ */
+class TakeStepProbabilities : public TakeStep {
+private:
+    std::vector<std::shared_ptr<TakeStep> > m_steps;
+    std::vector<double> m_weights;
+    std::shared_ptr<std::discrete_distribution<size_t> > m_distribution;
+    std::mt19937_64 m_generator;
+    size_t m_current_step;
+public:
+    virtual ~TakeStepProbabilities() {}
+    TakeStepProbabilities(const size_t seed);
+    void add_step(std::shared_ptr<TakeStep> step_input, const double weight_input=1);
+    void displace(pele::Array<double>& coords, MC* mc);
+    void report(pele::Array<double>& old_coords, const double old_energy,
+            pele::Array<double>& new_coords, const double new_energy,
+            const bool success, MC* mc);
+    std::vector<double> get_weights() const { return m_weights; }
+};
+
+} // namespace mcpele
+
+#endif // #ifndef _MCPELE_TAKE_STEP_PROBABILITIES_H__
---
+++ source/take_step_probabilities.cpp
@@ -0,0 +1,36 @@
+#include "mcpele/take_step_probabilities.h"
+
+namespace mcpele {
+
+TakeStepProbabilities::TakeStepProbabilities(const size_t seed)
+    : m_generator(seed)
+{}
+
+void TakeStepProbabilities::add_step(std::shared_patr<TakeStep> step_input,
+        const double weight_input)
+{
+    m_steps.push_back(step_input);
+    m_weights.push_back(weight_input);
+    m_steps.swap(m_steps);
+    m_weights.swap(m_weights);
+    m_distribution = std::make_shared<std::discrete_distribution<size_t> >(m_weights.begin(), m_weights.end());
+}
+
+void TakeStepProbabilities::displace(pele::Array<double>& coords, MC* mc)
+{
+    if (m_steps.size() == 0) {
+        throw std::runtime_error("TakeStepProbabilities::displace: no step specified");
+    }
+    m_current_step = m_distribution->(m_generator);
+    m_steps.at(m_current_step).displace(coords, mc);
+}
+
+void report(pele::Array<double>& old_coords, const double old_energy,
+        pele::Array<double>& new_coords, const double new_energy,
+        const bool success, MC* mc)
+{
+    m_steps.at(m_current_step).report(old_coords, old_energy, new_coords,
+            new_energy, success, mc);
+}
+
+} // namespace mcpele
