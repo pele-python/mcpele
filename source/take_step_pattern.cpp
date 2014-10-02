059bd2548072405f0e621e8b75c2314ce41c53a8
Julian Schrenk <kjs73@cam.ac.uk>
2014-10-02 13:51:10 +0100

    fix bug in takestep pattern, increment should go before displace

--- source/take_step_pattern.cpp
+++ source/take_step_pattern.cpp
@@ -4,8 +4,8 @@ namespace mcpele {
 
 void TakeStepPattern::displace(pele::Array<double>& coords, MC* mc)
 {
-    m_steps.get_step_index()->displace(coords, mc);
     ++m_steps;
+    m_steps.get_step_index()->displace(coords, mc);
 }
 
 } // namespace mcpele
