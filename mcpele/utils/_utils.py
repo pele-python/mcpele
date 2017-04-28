daca73f92c92446eaa5ec0b65bb5e5c20af88cef
Johannes Klicpera <j.klicpera@mailbox.org>
2017-04-28 15:35:54 +0100

    Rename hf5 to hdf5

--- mcpele/utils/_utils.py
+++ mcpele/utils/_utils.py
@@ -2,9 +2,9 @@ from __future__ import division
 import numpy as np
 import pandas as pd
 
-def write_2d_array_to_hf5(array, key, path):
+def write_2d_array_to_hdf5(array, key, path):
     """
-    this function can be used to dump any 2d array to a hf5 database
+    this function can be used to dump any 2d array to a hdf5 database
     use this to dump a trajectory to a database from python
     """
     assert array.ndim == 2
@@ -14,9 +14,9 @@ def write_2d_array_to_hf5(array, key, path):
     df = pd.DataFrame(np.array(array), index=ind, columns=col)
     df.to_hdf(path, key)
 
-def read_hf5_to_2d_array(path, key):
+def read_hdf5_to_2d_array(path, key):
     """
-    read a 2d array from a hf5 database
+    read a 2d array from a hdf5 database
     """
     df = pd.read_hdf(path, key)
     array = np.array(df.values)
