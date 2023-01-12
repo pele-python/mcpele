964dba47d4525a0396d61075493e8e1fa78c21aa
Praharsh Suryadevara <praharsharmm@gmail.com>
2023-01-12 17:36:28 -0500

    add option for max stepsize

--- source/mcpele/random_coords_displacement.h
+++ source/mcpele/random_coords_displacement.h
@@ -15,16 +15,20 @@ namespace mcpele {
  */
 class RandomCoordsDisplacement : public TakeStep {
 protected:
-    size_t m_seed;
-    std::mt19937_64 m_generator;
-    std::uniform_real_distribution<double> m_real_distribution;
-    double m_stepsize;
-    size_t m_count;
+  size_t m_seed;
+  std::mt19937_64 m_generator;
+  std::uniform_real_distribution<double> m_real_distribution;
+  double m_stepsize;
+  size_t m_count;
+  // if max stepsize is 0, then no max stepsize is set
+  double m_max_stepsize;
+
 public:
-    RandomCoordsDisplacement(const size_t rseed, const double stepsize=1);
-    virtual ~RandomCoordsDisplacement() {}
-    virtual void displace(pele::Array<double>& coords, MC* mc) =0;
-    size_t get_seed() const {return m_seed;}
+  RandomCoordsDisplacement(const size_t rseed, const double stepsize = 1,
+                           const double max_stepsize = 0);
+  virtual ~RandomCoordsDisplacement() {}
+  virtual void displace(pele::Array<double> &coords, MC *mc) = 0;
+  size_t get_seed() const { return m_seed; }
   void set_generator_seed(const size_t inp) { m_generator.seed(inp); }
   double expected_mean() const { return 0; }
   double get_stepsize() const { return m_stepsize; }
@@ -32,8 +36,15 @@ public:
   /**
    * Reference: http://mathworld.wolfram.com/UniformDistribution.html
    */
-  double expected_variance(const double ss) const { return ss * ss / static_cast<double>(12); }
-  void increase_acceptance(const double factor) { m_stepsize *= factor; }
+  double expected_variance(const double ss) const {
+    return ss * ss / static_cast<double>(12);
+  }
+  void increase_acceptance(const double factor) {
+    // 0 means the max stepsize has not been set
+    if (m_max_stepsize == 0 or m_stepsize < m_max_stepsize) {
+      m_stepsize *= factor;
+    }
+  }
   void decrease_acceptance(const double factor) { m_stepsize /= factor; }
   size_t get_count() const { return m_count; }
   void set_count(const size_t input) { m_count = input; }
@@ -41,32 +52,45 @@ public:
 
 class
 
-RandomCoordsDisplacementAll : public RandomCoordsDisplacement {
+    RandomCoordsDisplacementAll : public RandomCoordsDisplacement {
   std::vector<long> m_changed_atoms;
   std::vector<double> m_changed_coords_old;
+
 public:
-  RandomCoordsDisplacementAll(const size_t rseed, const double stepsize=1);
-  // The overloaded constructor helps return changed atoms and particles for 
-  RandomCoordsDisplacementAll(const size_t rseed, const size_t nparticles, const size_t ndim, const double stepsize=1);
+  RandomCoordsDisplacementAll(const size_t rseed, const double stepsize = 1,
+                              double max_stepsize = 0);
+  // The overloaded constructor helps return changed atoms and particles for
+  RandomCoordsDisplacementAll(const size_t rseed, const size_t nparticles,
+                              const size_t ndim, const double stepsize = 1,
+                              double max_stepsize = 0);
   virtual ~RandomCoordsDisplacementAll() {}
-  virtual void displace(pele::Array<double>& coords, MC* mc);
+  virtual void displace(pele::Array<double> &coords, MC *mc);
   const std::vector<long> get_changed_atoms() const { return m_changed_atoms; }
-  const std::vector<double> get_changed_coords_old() const { return m_changed_coords_old; }
-  
+  const std::vector<double> get_changed_coords_old() const {
+    return m_changed_coords_old;
+  }
 };
 
 class RandomCoordsDisplacementSingle : public RandomCoordsDisplacement {
-    size_t m_nparticles, m_ndim;
-    std::uniform_int_distribution<size_t> m_int_distribution;
-    std::vector<long> m_changed_atoms = std::vector<long>(1);
-    std::vector<double> m_changed_coords_old;
+  size_t m_nparticles, m_ndim;
+  std::uniform_int_distribution<size_t> m_int_distribution;
+  std::vector<long> m_changed_atoms = std::vector<long>(1);
+  std::vector<double> m_changed_coords_old;
+  double max_stepsize;
+
 public:
-    RandomCoordsDisplacementSingle(const size_t rseed, const size_t nparticles, const size_t ndim, const double stepsize=1);
-    virtual ~RandomCoordsDisplacementSingle() {}
-    virtual void displace(pele::Array<double>& coords, MC* mc);
-    size_t get_rand_particle(){ return m_changed_atoms[0]; } //dangerous function, should be used only for testing purposes
+  RandomCoordsDisplacementSingle(const size_t rseed, const size_t nparticles,
+                                 const size_t ndim, const double stepsize = 1,
+                                 const double max_stepsize = 0.0);
+  virtual ~RandomCoordsDisplacementSingle() {}
+  virtual void displace(pele::Array<double> &coords, MC *mc);
+  size_t get_rand_particle() {
+    return m_changed_atoms[0];
+  } // dangerous function, should be used only for testing purposes
   const std::vector<long> get_changed_atoms() const { return m_changed_atoms; }
-  const std::vector<double> get_changed_coords_old() const { return m_changed_coords_old; }
+  const std::vector<double> get_changed_coords_old() const {
+    return m_changed_coords_old;
+  }
 };
 
 } // namespace mcpele
--- source/random_coords_displacement.cpp
+++ source/random_coords_displacement.cpp
@@ -1,71 +1,66 @@
 #include "mcpele/random_coords_displacement.h"
+#include <ostream>
 
 namespace mcpele {
 
 /*RandomCoordsDisplacement*/
 
-  RandomCoordsDisplacement::RandomCoordsDisplacement(const size_t rseed, const double stepsize)
-    : m_seed(rseed),
-      m_generator(rseed),
-      m_real_distribution(0.0, 1.0),
-      m_stepsize(stepsize),
-      m_count(0)
-  {
+RandomCoordsDisplacement::RandomCoordsDisplacement(const size_t rseed,
+                                                   const double stepsize,
+                                                   const double max_stepsize)
+    : m_seed(rseed), m_generator(rseed), m_real_distribution(0.0, 1.0),
+      m_stepsize(stepsize), m_count(0) {
 #ifdef DEBUG
-    std::cout<<"seed TakeStep:"<<_seed<< "\n";
+  std::cout << "seed TakeStep:" << _seed << "\n";
 #endif
+}
+
+/*RandomCoordsDisplacementAll*/
+
+RandomCoordsDisplacementAll::RandomCoordsDisplacementAll(
+    const size_t rseed, const double stepsize, const double max_stepsize)
+    : RandomCoordsDisplacement(rseed, stepsize, max_stepsize) {}
+
+RandomCoordsDisplacementAll::RandomCoordsDisplacementAll(
+    const size_t rseed, const size_t nparticles, const size_t ndim,
+    const double stepsize, const double max_stepsize)
+    : RandomCoordsDisplacement(rseed, stepsize, max_stepsize),
+      m_changed_atoms(nparticles), m_changed_coords_old(nparticles * ndim) {}
+
+void RandomCoordsDisplacementAll::displace(pele::Array<double> &coords,
+                                           MC *mc) {
+  std::iota(m_changed_atoms.begin(), m_changed_atoms.end(), 0);
+  if (m_changed_coords_old.size() != 0) {
+    std::copy(coords.begin(), coords.end(), m_changed_coords_old.begin());
   }
 
-  /*RandomCoordsDisplacementAll*/
-
-  RandomCoordsDisplacementAll::RandomCoordsDisplacementAll(const size_t rseed, const double stepsize)
-    : RandomCoordsDisplacement(rseed, stepsize)
-  {}
-
-  RandomCoordsDisplacementAll::RandomCoordsDisplacementAll(const size_t rseed, const size_t nparticles, const size_t ndim, const double stepsize)
-    : RandomCoordsDisplacement(rseed, stepsize),
-      m_changed_atoms(nparticles),
-      m_changed_coords_old(nparticles*ndim)
-  {}
-  
-  
-
-  void RandomCoordsDisplacementAll::displace(pele::Array<double>& coords, MC* mc)
-  { std::iota(m_changed_atoms.begin(), m_changed_atoms.end(), 0);
-    if (m_changed_coords_old.size() != 0) {
-      std::copy(coords.begin(), coords.end(), m_changed_coords_old.begin());
-    }
-      
-    for (size_t i = 0; i < coords.size(); ++i) {
-      double rand = m_real_distribution(m_generator);
-      coords[i] += (0.5 - rand) * m_stepsize;
-    }
-    ++m_count;
+  for (size_t i = 0; i < coords.size(); ++i) {
+    double rand = m_real_distribution(m_generator);
+    coords[i] += (0.5 - rand) * m_stepsize;
   }
+  ++m_count;
+}
 
-  /*RandomCoordsDisplacementSingle*/
+/*RandomCoordsDisplacementSingle*/
 
-  RandomCoordsDisplacementSingle::RandomCoordsDisplacementSingle(const size_t rseed, const size_t nparticles, const size_t ndim, const double stepsize)
-    : RandomCoordsDisplacement(rseed, stepsize),
-      m_nparticles(nparticles),
-      m_ndim(ndim),
-      m_int_distribution(0, m_nparticles-1),
-      m_changed_coords_old(ndim)
-    {}
+RandomCoordsDisplacementSingle::RandomCoordsDisplacementSingle(
+    const size_t rseed, const size_t nparticles, const size_t ndim,
+    const double stepsize, const double max_stepsize)
+    : RandomCoordsDisplacement(rseed, stepsize, max_stepsize),
+      m_nparticles(nparticles), m_ndim(ndim),
+      m_int_distribution(0, m_nparticles - 1), m_changed_coords_old(ndim) {}
 
-  
-  void RandomCoordsDisplacementSingle::displace(pele::Array<double>& coords, MC* mc)
-  {
-    m_changed_atoms[0] = m_int_distribution(m_generator);
-    // std::cout << m_changed_atoms[0] << "m_changed_atoms \n";
-    // std::cout << m_generator << " M generaaator \n";
-    size_t offset = m_changed_atoms[0] * m_ndim;
-    for (size_t i = 0; i < m_ndim; ++i) {
-      double rand = m_real_distribution(m_generator);
-      m_changed_coords_old[i] = coords[offset + i];
-      coords[offset + i] += (0.5 - rand) * m_stepsize;
-    }
-    ++m_count;
+void RandomCoordsDisplacementSingle::displace(pele::Array<double> &coords,
+                                              MC *mc) {
+  m_changed_atoms[0] = m_int_distribution(m_generator);
+  size_t offset = m_changed_atoms[0] * m_ndim;
+  std::cout << "stepsize" << m_stepsize << std::endl;
+  for (size_t i = 0; i < m_ndim; ++i) {
+    double rand = m_real_distribution(m_generator);
+    m_changed_coords_old[i] = coords[offset + i];
+    coords[offset + i] += (0.5 - rand) * m_stepsize;
   }
+  ++m_count;
+}
 
 } // namespace mcpele
